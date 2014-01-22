4c567b9986891e08b4a5d1aa8f29884a3bfdaca4
Vincent Degroote <vincent.degroote@canaltp.fr>
2014-01-22 02:04:04 +0100

    METH-37 left menu without links

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -14,4 +14,21 @@ class DefaultController extends Controller
 
         return $this->render('CanalTPMethBundle:Default:index.html.twig', array('networks' => $networks));
     }
+    
+    public function navigationAction()
+    {
+        $meth_navitia = $this->get('canal_tp_meth.navitia');
+        $networks = $this->getDoctrine()
+        ->getRepository('CanalTPMethBundle:Network', 'meth')
+        ->findNetworksByUserId($this->getUser()->getId());
+
+         // Configuration
+        $result = $meth_navitia->getLinesByMode($networks[0]['coverage_id'], $networks[0]['name_id']);
+        // var_dump($result);die;
+        // var_dump($result);die;
+        return $this->render(
+            'CanalTPMethBundle:Default:navigation.html.twig',
+            array('result' => $result)
+        );
+    }
 }
