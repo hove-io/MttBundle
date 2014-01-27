a8e75e6d6a0bc7ba12576b9457b824f742ccb2d0
Vincent Degroote <vincent.degroote@canaltp.fr>
2014-01-27 21:34:10 +0100

    Add form to edit a block

---
+++ Controller/BlockController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CanalTP\MethBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class BlockController extends Controller
+{
+    /*
+     * @function display a form to choose a layout for a given line or save this form and redirects
+     */
+    public function getFormAction($block_type = 'text', $dom_id)
+    {
+        $defaultData = array();
+        $form = $this->createFormBuilder($defaultData)
+            ->add('title', 'text')
+            ->add('content', 'textarea', array('attr' => array('rows' => 5)))
+            ->add('_dom_id', 'hidden', array('data' => $dom_id))
+            ->setAction($this->getRequest()->getRequestUri())
+            ->setMethod('POST')
+            ->getForm();
+            
+        $form->handleRequest($this->getRequest());
+
+        if ($form->isValid()) 
+        {
+            //TODO save data
+            $data = $form->getData();
+        }
+        else
+        {
+            return $this->render(
+                'CanalTPMethBundle:Block:get_form.html.twig',
+                array(
+                    'form'        => $form->createView(),
+                )
+            );
+        }
+    }
+}
--- Resources/public/js/layout.js
+++ Resources/public/js/layout.js
@@ -1,11 +1,32 @@
-define(['jquery', 'bootstrap'], function($) {
+define(['jquery'], function($) {
     var layout = {};
+    
+    var $icon_tpl = $('<span class="glyphicon"></span>');
     
     layout.init = function($wrapper)
     {
         $('.block').each(function(){
-            var data = $(this).data();
-            console.dir(data);
+           $elem = $(this);
+           var data = $elem.data();
+           var icon_class = 'glyphicon-';
+            if (data.blockLevel == "line" && data.blockType == "text")
+            {
+                icon_class += 'edit';
+                // listener
+                $elem.click(function(){
+                    var url = Routing.generate('canal_tp_meth_block_get_form', { 'block_type': data.blockType, 'dom_id' : $elem.attr('id')});
+                    $('#base-modal').modal({
+                        keyboard:true,
+                        remote: url
+                    });
+                });
+            }
+            else
+            {
+                icon_class += 'ban-circle';
+                $elem.addClass('disabled-block');
+            }
+            $elem.append($icon_tpl.clone().addClass(icon_class));
         });
     };
     
