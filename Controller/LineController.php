a2c22aba912cdf2a593d1671db2faec0d2ae4f17
Rémy Abi-Khalil <remyabi@gmail.com>
2014-01-27 15:18:04 +0100

    Controller of Line updated.

--- Controller/LineController.php
+++ Controller/LineController.php
@@ -9,54 +9,57 @@ use CanalTP\MethBundle\Entity\Line;
 
 class LineController extends Controller
 {
+    private function processForm($form, $line, $params)
+    {
+        if (empty($line))
+        {
+            $data = $form->getData();
+            $line = new Line();
+            $line->setCoverageId($params['coverageId']);
+            $line->setNetworkId($params['networkId']);
+            $line->setNavitiaLineId($params['navitiaLineId']);
+            $line->setLayout($data['layout']);
+        }
+        if ($line->getLayout() != null)
+        {
+            $em = $this->getDoctrine()->getManager('meth');
+            $em->persist($line);
+            $em->flush();
+                
+            $this->get('session')->getFlashBag()->add('notice', 'line.flash.layout_chosen');
+        }
+        return $this->redirect($this->generateUrl('canal_tp_meth_stop_point_list', $params));
+    }
+
     /*
      * @function display a form to choose a layout for a given line or save this form and redirects
      */
     public function chooseLayoutAction($coverage_id, $network_id, $line_id, $route_id)
     {
-        $line = $this->getDoctrine()->getRepository('CanalTPMethBundle:Line', 'meth')->findOneBy(array(
-            'coverageId'   => $coverage_id,
-            'networkId'    => $network_id,
-            'navitiaLineId'=> $line_id,
-        ));
-        $url_params = array(
-            'coverage_id'   => $coverage_id,
-            'network_id'    => $network_id,
-            'line_id'=> $line_id,
-            'route_id'=> $route_id,
+
+        $params = array('coverage_id'   => $coverage_id,
+                        'network_id'    => $network_id,
+                        'line_id'       => $line_id,
+                        'route_id'      => $route_id);
+        $line = $this->getDoctrine()
+                ->getRepository('CanalTPMethBundle:Line', 'meth')
+                ->findOneBy(array('coverageId'   => $coverage_id,
+                                  'networkId'    => $network_id,
+                                  'navitiaLineId'=> $line_id)
         );
+
         $form = $this->createFormBuilder($line)
             ->add('layout', 'layout', array(
                 'empty_value' => 'Choose a layout',
             ))
-            ->setAction($this->generateUrl('canal_tp_meth_line_choose_layout', $url_params))
+            ->setAction($this->getRequest()->getRequestUri())
             ->setMethod('POST')
             ->getForm();
+
         $form->handleRequest($this->getRequest());
         if ($form->isValid())
-        {
-            if (empty($line))
-            {
-                $data = $form->getData();
-                $line = new Line();
-                $line->setCoverageId($coverage_id);
-                $line->setNetworkId($network_id);
-                $line->setNavitiaLineId($line_id);
-                $line->setLayout($data['layout']);
-            }
-            // $layout
-            if ($line->getLayout() != null)
-            {
-                $em = $this->getDoctrine()->getManager('meth');
-                $em->persist($line);
-                $em->flush();
-                
-                $this->get('session')->getFlashBag()->add('notice', 'line.flash.layout_chosen');
-            }
-            return $this->redirect($this->generateUrl('canal_tp_meth_stop_point_list', $url_params));
-        }
+            return ($this->processForm($form, $line, $params));
         else
-        {
             return $this->render(
                 'CanalTPMethBundle:Line:chooseLayout.html.twig',
                 array(
@@ -64,7 +67,6 @@ class LineController extends Controller
                     'line_layout' => false
                 )
             );
-        }
     }
     
     /*
