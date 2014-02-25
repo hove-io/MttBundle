097aba96216beca23a3e5afdef24d5ae66f66d5a
Vincent Degroote <vincent.degroote@canaltp.fr>
2014-02-25 10:32:40 +0100

    METH-48 check if a block is higher than expected by the parent layout

--- Resources/public/js/layout/validator.js
+++ Resources/public/js/layout/validator.js
@@ -1,9 +1,8 @@
-define(['jquery', 'bootstrap'], function($) {
+define(['jquery', 'mtt/translations/default'], function($) {
     var validator = {};
     var $wrapper,
-        $msgWrapperTpl = $('<div class="alert alert-danger alert-dismissable"></div>');
+        $msgWrapperTpl = $('<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button></div>');
     ;
-    
     
     validator.init = function(params){
         $wrapper = params.wrapper;
@@ -13,14 +12,27 @@ define(['jquery', 'bootstrap'], function($) {
     
     validator.validate = function(){
         var errors = [];
+        var msg_added = false;
         // check if content is bigger than block wrapper
         $wrapper.find('.block > *[data-validate-height="1"]').each(function(){
             if ($(this).children().height() > $(this).height())
             {
                 $(this).parents('.block').addClass('error');
-                errors.push();
+                if (msg_added == false)
+                {
+                    errors.push(Translator.trans('calendar.error.content_higher_than_wrapper', {}, 'default'));
+                    msg_added = true;
+                }
             }
         });
+        if (errors.length > 0)
+        {
+            for (var error in errors)
+            {
+                $msgWrapperTpl.append('<div>' + errors[error] + '</div>');
+            }
+            $('.navbar').append($msgWrapperTpl);
+        }
     };
     
     return validator;
