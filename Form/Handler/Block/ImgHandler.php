6224cce773326ff0ac3451df24cad28db8f903dc
Rémy Abi-Khalil <remyabi@gmail.com>
2014-03-03 17:24:29 +0100

    MediaManager updated.

--- Form/Handler/Block/ImgHandler.php
+++ Form/Handler/Block/ImgHandler.php
@@ -7,11 +7,14 @@ use Symfony\Component\Filesystem\Filesystem;
 use Doctrine\Common\Persistence\ObjectManager;
 use CanalTP\MediaManager\Category\CategoryType;
 use CanalTP\MediaManagerBundle\DataCollector\MediaDataCollector as MediaManager;
+use CanalTP\MediaManagerBundle\Entity\Category;
 use CanalTP\MediaManagerBundle\Entity\Media;
-use CanalTP\MethBundle\Entity\Block;
-
+use CanalTP\MethBundle\Entity\Block
+;
 class ImgHandler extends AbstractHandler
 {
+    const ID_LINE_MAP = 'line_map';
+
     private $co = null;
     private $lastImgPath = null;
     private $mediaManager = null;
@@ -43,16 +46,16 @@ class ImgHandler extends AbstractHandler
 
     public function process(Block $formBlock, $timetable)
     {
-        $media = new Media(
-            // TODO: get network id
-            CategoryType::NETWORK,
-            $timetable->getId(),
-            // TODO: change Category Type to Route
-            CategoryType::LINE,
-            $timetable->getExternalRouteId()
-        );
+        $timetableCategory = new Category($timetable->getId(), CategoryType::NETWORK);
+        $routeCategory = new Category($timetable->getExternalRouteId(), CategoryType::LINE);
+        $media = new Media();
 
+        $routeCategory->setParent($timetableCategory);
+        $media->setCategory($routeCategory);
         $media->setFile($formBlock->getContent());
+        // TODO: Give img_label -> "ImgHandler::ID_LINE_MAP" as parameter
+        $media->setFileName(ImgHandler::ID_LINE_MAP);
+
         $this->mediaManager->save($media);
         $formBlock->setContent($this->mediaManager->getUrlByMedia($media));
         $this->saveBlock($formBlock, $timetable);
