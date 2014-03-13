0f5158ea52f1aced277e78e10de44c0750206bb7
Rémy Abi-Khalil <remyabi@gmail.com>
2014-03-13 16:42:19 +0100

    Refs #METH-77, Form of season updated. (without validation)

--- Form/Type/SeasonType.php
+++ Form/Type/SeasonType.php
@@ -8,20 +8,15 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class SeasonType extends AbstractType
 {
-    private function buildBusinessRightForm(FormBuilderInterface $builder)
-    {
-        $builder->add('title', 'text');
-        $builder->add('startDate', 'date');
-        $builder->add('endDate', 'date');
-    }
-
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $this->buildBusinessRightForm($builder);
+        $builder->add('title', 'text');
+        $builder->add('startDate', 'date');
+        $builder->add('endDate', 'date');
         $builder->setAction($options['action']);
     }
 
