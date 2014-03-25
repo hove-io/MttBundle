6d7239f22b5ec733bb134dab7d103112024d3043
dql <david.quintanel@canaltp.fr>
2014-03-25 16:32:21 +0100

    changed user entity namespace

--- DataFixtures/ORM/FixtureUser.php
+++ DataFixtures/ORM/FixtureUser.php
@@ -6,7 +6,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 
-use CanalTP\SamCoreBundle\Entity\User;
+use CanalTP\SamEcoreUserManagerBundle\Entity\User;
 use CanalTP\MttBundle\Entity\Network;
 
 
