f1f897d0c76898eaa7a756c71f38e6f1a8416aa0
Vincent Degroote <vincent.degroote@canaltp.fr>
2014-04-04 15:25:40 +0200

    METH-121 Functionnal test on Route level

--- Tests/Functional/Controller/TimetableControllerTest.php
+++ Tests/Functional/Controller/TimetableControllerTest.php
@@ -7,7 +7,7 @@ use CanalTP\MttBundle\Tests\DataFixtures\ORM\Fixture;
 
 class TimetableControllerTest extends AbstractControllerTest
 {
-    private function getViewRoute($seasonId = 1)
+    private function getViewRoute($seasonId, $externalStopPointId = 'stop_point:TTR:SP:STPGB-2')
     {
         return $this->generateRoute(
             'canal_tp_mtt_timetable_view',
@@ -15,7 +15,7 @@ class TimetableControllerTest extends AbstractControllerTest
                 'externalNetworkId' => 'network:Filbleu',
                 'externalLineId' => 'line:TTR:Nav62',
                 'externalRouteId' => 'route:TTR:Nav155',
-                'externalStopPointId' => 'stop_point:TTR:SP:STPGB-2',
+                'externalStopPointId' => $externalStopPointId,
                 "seasonId" => $seasonId
             )
         );
@@ -23,14 +23,25 @@ class TimetableControllerTest extends AbstractControllerTest
     
     public function testSeasonBlockDates()
     {
-        $crawler = $this->doRequestRoute($this->getViewRoute());
+        $season = $this->getRepository('CanalTPMttBundle:Season')->find(1);
+        // check on stopPoint page
+        $crawler = $this->doRequestRoute($this->getViewRoute($season->getId()));
+        $this->checkBlockAndDates($crawler, $season);
+        // check on route page
+        $crawler = $this->doRequestRoute($this->getViewRoute($season->getId(), false));
+        $this->checkBlockAndDates($crawler, $season);
+    }
+    
+    private function checkBlockAndDates($crawler, $season)
+    {
         // Title present
         $blockTitle = $this->client->getContainer()->get('translator')->trans('season.block.title', array(), 'default');
         $this->assertTrue($crawler->filter('html:contains("' . $blockTitle . '")')->count() == 1);
-        $season = $this->getRepository('CanalTPMttBundle:Season')->find(1);
+        // TODO: Retrieve locale client.... returns null: $this->client->getContainer()->get('session')->get('_locale');
         $fmt = datefmt_create('fr_FR', \IntlDateFormatter::LONG, \IntlDateFormatter::NONE);
+        // dates compliant
         $this->assertTrue($crawler->filter('html:contains("' . datefmt_format($fmt, $season->getStartDate()) . '")')->count() == 1);
         $this->assertTrue($crawler->filter('html:contains("' . datefmt_format($fmt, $season->getEndDate()) . '")')->count() == 1);
-        
     }
+    
 }
