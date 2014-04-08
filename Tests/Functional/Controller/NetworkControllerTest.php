8acf4acfe59d4ddbbb355ce55a4f10c6be113054
Rémy Abi-Khalil <remyabi@gmail.com>
2014-04-08 11:21:26 +0200

    Refs #METH-144 ~ Some Tests implemented

---
+++ Tests/Functional/Controller/NetworkControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CanalTP\MttBundle\Tests\Functional\Controller;
+
+use CanalTP\MttBundle\Tests\DataFixtures\ORM\Fixture;
+
+class NetworkControllerTest extends AbstractControllerTest
+{
+    public function testList()
+    {
+        $route = $this->generateRoute('canal_tp_mtt_network_list');
+        $crawler = $this->doRequestRoute($route);
+    }
+    
+    public function testEdit()
+    {
+        $route = $this->generateRoute('canal_tp_mtt_network_edit');
+        $crawler = $this->doRequestRoute($route);
+
+        $labelNetworkField = $crawler->filter('select#mtt_network_external_id')->siblings()->filter('label[style="display:none;"]')->count();
+        $selectNetworkField = $crawler->filter('select#mtt_network_external_id[style="display:none;"]')->count();
+
+        $this->assertTrue(
+            ($selectNetworkField == 1 && $labelNetworkField == 1),
+            "field of network should be hidden before we choose coverage. Expected hidden. hident ? :$selectNetworkField"
+        );
+    }
+}
