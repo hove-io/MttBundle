da51e0ba060dec00bdf906b197588f73b0577f6f
Vincent Degroote <vincent.degroote@canaltp.fr>
2014-04-14 12:36:48 +0200

    METH-145 new constraint to validate a field is greather than an other.

---
+++ Validator/Constraints/GreaterThanField.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CanalTP\MttBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+class GreaterThanField extends Constraint
+{
+    public $field = null;
+    public $message = null;
+
+    public function validatedBy()
+    {
+        return 'validator_constraints_greater_than_field';
+    }
+}
---
+++ Validator/Constraints/GreaterThanFieldValidator.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace CanalTP\MttBundle\Validator\Constraints;
+
+use Symfony\Bundle\FrameworkBundle\Translation\Translator;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * GreaterThanFieldValidator
+ */
+class GreaterThanFieldValidator extends ConstraintValidator
+{
+    private $translator = null;
+
+    public function __construct(Translator $translator)
+    {
+        $this->translator = $translator;
+    }
+    
+    /**
+     * Checks if the passed value is valid.
+     *
+     * @param mixed      $value      The value that should be validated
+     * @param Constraint $constraint The constrain for the validation
+     *
+     * @return Boolean Whether or not the value is valid
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        if ($value < $this->context->getRoot()->get($constraint->field)->getData()) {
+ 
+            $this->context->addViolation(
+                'error.greater_than_field', 
+                array(
+                    '%field%' => $constraint->field,
+                )
+            );
+ 
+            return false;
+        }
+ 
+        return true;
+    }
+}
