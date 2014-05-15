61d3b5116ca41114d262ced62190e66104fcf913
Rémy Abi-Khalil <remyabi@gmail.com>
2014-05-15 18:24:59 +0200

    DataFixture for test updated.

--- DataFixtures/ORM/FixtureMtt.php
+++ DataFixtures/ORM/FixtureMtt.php
@@ -3,37 +3,47 @@
 namespace CanalTP\MttBundle\DataFixtures\ORM;
 
 use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\Common\DataFixtures\AbstractFixture;
-use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 
-use CanalTP\SamEcoreUserManagerBundle\Entity\User;
-use CanalTP\MttBundle\Entity\Network;
-use CanalTP\MttBundle\Entity\Layout;
+use CanalTP\SamBundle\Tests\DataFixtures\ORM\Fixture as SamBaseFixture;
+// use CanalTP\MttBundle\Entity\Network;
+// use CanalTP\MttBundle\Entity\Layout;
 
 
-class FixturesUser extends AbstractFixture implements OrderedFixtureInterface
+class FixturesUser extends SamBaseFixture
 {
     private $em = null;
 
-    private function createUser($data)
-    {
-        //  On crée l'utilisateur admin akambi
-        $user = new User();
-        $user->setUsername($data['username']);
-        $user->setFirstName($data['firstname']);
-        $user->setLastName($data['lastname']);
-        $user->setEnabled(true);
-        $user->setEmail($data['email']);
-        $user->setPlainPassword($data['password']);
+     const ROLE_USER_MTT  = 'ROLE_USER_MTT';
+     const ROLE_ADMIN_MTT = 'ROLE_ADMIN_MTT';
 
-        $this->addReference('user-'.$data['username'], $user);
+     protected $users = array(
+       'mtt' => array(
+            'id'        => null,
+            'username'  => 'mtt',
+            'firstname' => 'mtt_firstname',
+            'lastname'  => 'mtt_lastname',
+            'email'     => 'mtt@canaltp.fr',
+            'password'  => 'mtt',
+            'roles'     => array('role-admin-mtt', 'role-user-mtt')
+        )
+     );
 
-        foreach ($data['roles'] as $role) {
-            $user->addUserRole($this->getReference($role));
-        }
-        $this->em->persist($user);
-        return ($user);
-    }
+
+    protected $userPermissions = array(
+        'BUSINESS_GENERATE_PDF',
+        'BUSINESS_GENERATE_DISTRIBUTION_LIST_PDF'
+    );
+
+    protected $adminPermissions = array(
+        'BUSINESS_VIEW_NAVITIA_LOG',
+        'BUSINESS_CHOOSE_LAYOUT',
+        'BUSINESS_ASSIGN_NETWORK_LAYOUT',
+        'BUSINESS_EDIT_LAYOUT',
+        'BUSINESS_MANAGE_SEASON',
+        'BUSINESS_MANAGE_DISTRIBUTION_LIST',
+        'BUSINESS_GENERATE_DISTRIBUTION_LIST_PDF',
+        'BUSINESS_GENERATE_PDF'
+    );
 
     private function createLayout($layoutProperties, $networks = array())
     {
@@ -69,29 +79,8 @@ class FixturesUser extends AbstractFixture implements OrderedFixtureInterface
         return ($network);
     }
 
-    public function load(ObjectManager $em)
+    private function createLayouts($network1, $network2)
     {
-        $this->em = $em;
-        $user = $this->createUser(
-            array(
-                'username' => 'mtt',
-                'firstname' => 'mtt_firstname',
-                'lastname' => 'mtt_lastname',
-                'email' => 'mtt@canaltp.fr',
-                'password' => 'mtt',
-                'roles' => array('role-admin-mtt', 'role-user-mtt')
-            )
-        );
-
-        $network = $this->createNetwork('network:Filbleu', '46cadd8a-e385-4169-9cb8-c05766eeeecb');
-        $network->addUser($user);
-        $network2 = $this->createNetwork('network:Agglobus');
-        $network2->addUser($user);
-        $network3 = $this->createNetwork('network:SNCF');
-        $network3->addUser($user);
-        $network4 = $this->createNetwork('network:RATP');
-        $network4->addUser($user);
-
         $layout1 = $this->createLayout(
             array(
                 'label'         => 'Layout 1 de type paysage (Dijon 1)',
@@ -101,7 +90,7 @@ class FixturesUser extends AbstractFixture implements OrderedFixtureInterface
                 'calendarStart' => 4,
                 'calendarEnd'   => 1,
             ),
-            array($network, $network2)
+            array($network1, $network2)
         );
         $layout2 = $this->createLayout(
             array(
@@ -114,9 +103,39 @@ class FixturesUser extends AbstractFixture implements OrderedFixtureInterface
             ),
             array($network)
         );
-        $this->em->persist($network);
+
+        $this->em->persist($network1);
         $this->em->persist($network2);
         $this->em->flush();
+    }
+
+    public function load(ObjectManager $em)
+    {
+        $this->em = $em;
+        $app = $this->createApplication('mtt', '/mtt');
+        $userRole    = $this->createApplicationRole('User Mtt',  self::ROLE_USER_MTT,  $app, $this->userPermissions);
+        $addminRole  = $this->createApplicationRole('Admin Mtt', self::ROLE_ADMIN_MTT, $app, $this->adminPermissions);
+        $network1 = $this->createNetwork('network:Filbleu', '46cadd8a-e385-4169-9cb8-c05766eeeecb');
+        $network2 = $this->createNetwork('network:Agglobus', '46cadd8a-e385-4169-9cb8-c05766eeeecb');
+        $network3 = $this->createNetwork('network:SNCF', '46cadd8a-e385-4169-9cb8-c05766eeeecb');
+        $network4 = $this->createNetwork('network:RATP', '46cadd8a-e385-4169-9cb8-c05766eeeecb');
+
+        //associer les utilisateurs avec l'application
+        foreach ($this->users as &$userData) {
+            $isAdmin = in_array(self::ROLE_ADMIN_MTT, $userData['roles']);
+
+            $userEntity = $this->createUser(
+                $userData,
+                (($isAdmin) ? array($addminRole) : array($userRole))
+            );
+            $userData['id'] = $userEntity->getId();
+
+            $network1->addUser($userEntity);
+            $network2->addUser($userEntity);
+            $network3->addUser($userEntity);
+            $network4->addUser($userEntity);
+        }
+        $this->createLayout($network1, $network2);
     }
 
     /**
