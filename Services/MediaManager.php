eee01ab1ae2f6ae4247e17bea930ff9b8bf73887
Remy Abi-Khalil <remyabi@gmail.com>
2014-07-02 16:30:20 +0200

    Refs #IUS-202 ~ Remove Media added with some refactoring.

--- Services/MediaManager.php
+++ Services/MediaManager.php
@@ -55,6 +55,7 @@ class MediaManager
 
         $media = new Media();
         $media->setCategory($seasonCategory);
+        $media->setCompany($this->mediaDataCollector->getCompany());
 
         return $media;
     }
@@ -99,7 +100,7 @@ class MediaManager
         $media = $this->getMedia($timetable);
         $media->setFileName($fileName);
         $media->setFile($file);
-        $this->mediaDataCollector->save($media);
+        $media = $this->mediaDataCollector->save($media);
 
         return ($media);
     }
