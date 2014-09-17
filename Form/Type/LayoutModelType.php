a376e050cf36a4761175c9171ec154827d88cc3f
kzi <kevin.ziemianski@canaltp.fr>
2014-09-17 07:43:09 +0200

    Add layout model and upload form

---
+++ Form/Type/LayoutModelType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace CanalTP\MttBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\File;
+
+class LayoutModelType extends AbstractType
+{
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            'file',
+            'file',
+            array(
+                'label' => 'layout_model.labels.archive_path',
+                'required' => true,
+                'constraints' => array(
+                    new File(
+                        array(
+                            'maxSize' => '20M',
+                            'mimeTypes' => 'application/zip'
+                        )
+                    )
+                )
+            )
+        );
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'data_class' => 'CanalTP\MttBundle\Model\LayoutModel'
+            )
+        );
+    }
+
+    public function getName()
+    {
+        return 'layout_config';
+    }
+}
