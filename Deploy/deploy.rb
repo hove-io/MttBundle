4040bba06606ef4f75662be282c9fd1b8637a2fb
kzi <kevin.ziemianski@canaltp.fr>
2014-10-07 16:07:04 +0200

    Fix generate pdf (bad orientation send) + add deploy for templates

--- Deploy/deploy.rb
+++ Deploy/deploy.rb
@@ -5,5 +5,18 @@ namespace :mtt do
     end
 end
 
+namespace :mtt do
+    desc "Simlinks for templates"
+    task :templates_symlinks, :roles => :supervisor do
+        run "mkdir -p #{mtt_storage_path}template/img"
+        run "mkdir -p #{mtt_storage_path}template/css"
+        run "mkdir -p #{mtt_storage_path}template/twig"
+        run "cd #{current_release}/vendor/canaltp/mtt-bundle/CanalTP/MttBundle/Resources/public/img && ln -s #{mtt_storage_path}template/img uploads"
+        run "cd #{current_release}/vendor/canaltp/mtt-bundle/CanalTP/MttBundle/Resources/public/css && ln -s #{mtt_storage_path}template/css uploads"
+        run "cd #{current_release}/vendor/canaltp/mtt-bundle/CanalTP/MttBundle/Resources/views/Layouts && ln -s #{mtt_storage_path}template/twig uploads"
+    end
+end
+
 after "deploy:create_symlink", "mtt:restart"
+after "post:composer", "mtt:templates_symlinks"
 after "deploy:rollback", "mtt:restart"
