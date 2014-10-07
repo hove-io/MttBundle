89bea8a86831bce4f41fbe90cd8d410497d42f47
kzi <kevin.ziemianski@canaltp.fr>
2014-10-07 18:00:07 +0200

    Update deploy config

--- Deploy/deploy.rb
+++ Deploy/deploy.rb
@@ -8,12 +8,12 @@ end
 namespace :mtt do
     desc "Simlinks for templates"
     task :templates_symlinks, :roles => :supervisor do
-        run "mkdir -p #{mtt_storage_path}template/img"
-        run "mkdir -p #{mtt_storage_path}template/css"
-        run "mkdir -p #{mtt_storage_path}template/twig"
-        run "cd #{current_release}/vendor/canaltp/mtt-bundle/CanalTP/MttBundle/Resources/public/img && ln -s #{mtt_storage_path}template/img uploads"
-        run "cd #{current_release}/vendor/canaltp/mtt-bundle/CanalTP/MttBundle/Resources/public/css && ln -s #{mtt_storage_path}template/css uploads"
-        run "cd #{current_release}/vendor/canaltp/mtt-bundle/CanalTP/MttBundle/Resources/views/Layouts && ln -s #{mtt_storage_path}template/twig uploads"
+        run "mkdir -p #{shared_path}/template/img"
+        run "mkdir -p #{shared_path}/template/css"
+        run "mkdir -p #{shared_path}/template/twig"
+        run "cd #{current_release}/vendor/canaltp/mtt-bundle/CanalTP/MttBundle/Resources/public/img && ls -l && ln -s #{shared_path}/template/img uploads"
+        run "cd #{current_release}/vendor/canaltp/mtt-bundle/CanalTP/MttBundle/Resources/public/css && ls -l && ln -s #{shared_path}/template/css uploads"
+        run "cd #{current_release}/vendor/canaltp/mtt-bundle/CanalTP/MttBundle/Resources/views/Layouts && ls -l && ln -s #{shared_path}/template/twig uploads"
     end
 end
 
