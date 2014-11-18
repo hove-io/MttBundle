0fbc4ba2c545c5f001ee59d065a1ac34abbec2da
Vincent Degroote <vincent.degroote@canaltp.fr>
2014-11-18 11:17:20 +0100

    FIX: images in layouts were not found when a base path was needed

--- Controller/TimetableController.php
+++ Controller/TimetableController.php
@@ -89,7 +89,7 @@ class TimetableController extends AbstractController
                 'editable'              => $editable,
                 'displayMenu'           => $displayMenu,
                 'templatePath'          => '@CanalTPMtt/Layouts/uploads/' . $timetable->getLineConfig()->getLayoutConfig()->getLayout()->getId() . '/',
-                'imgPath'               => '/bundles/canaltpmtt/img/uploads/' . $timetable->getLineConfig()->getLayoutConfig()->getLayout()->getId() . '/',
+                'imgPath'               => 'bundles/canaltpmtt/img/uploads/' . $timetable->getLineConfig()->getLayoutConfig()->getLayout()->getId() . '/',
                 'cssPath'               => 'bundles/canaltpmtt/css/uploads/' . $timetable->getLineConfig()->getLayoutConfig()->getLayout()->getId() . '/'
             )
         );
