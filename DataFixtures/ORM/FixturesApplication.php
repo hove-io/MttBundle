85903e2b000bb50a3a61244801b8075807914244
kzi <kevin.ziemianski@canaltp.fr>
2014-11-18 14:17:48 +0100

    remove 'use'

--- DataFixtures/ORM/FixturesApplication.php
+++ DataFixtures/ORM/FixturesApplication.php
@@ -3,12 +3,7 @@
 namespace CanalTP\MttBundle\DataFixtures\ORM;
 
 use Doctrine\Common\Persistence\ObjectManager;
-use CanalTP\SamEcoreUserManagerBundle\Entity\User;
-
 use CanalTP\SamCoreBundle\Tests\DataFixtures\ORM\Fixture as SamBaseFixture;
-use CanalTP\MttBundle\Entity\Network;
-use CanalTP\MttBundle\Entity\LayoutConfig;
-use CanalTP\MttBundle\Entity\Layout;
 use CanalTP\SamCoreBundle\DataFixtures\ORM\ApplicationTrait;
 
 class FixturesApplication extends SamBaseFixture
