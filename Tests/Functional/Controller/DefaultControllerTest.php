8a637590bd1201dc733cae6cf2cf45a931a11252
Rémy Abi-Khalil <remyabi@gmail.com>
2014-11-19 14:22:51 +0100

    Refs #METH-356 ~ Fixtures for test updated (Default Controller)

--- Tests/Functional/Controller/DefaultControllerTest.php
+++ Tests/Functional/Controller/DefaultControllerTest.php
@@ -29,20 +29,10 @@ class DefaultControllerTest extends AbstractControllerTest
         $this->assertTrue($modes_count > 0, "Expected at least one mode for this user. Found :$modes_count");
     }
 
-    private function insertNetwork()
-    {
-        $fixture = new Fixture();
-        $network = $fixture->createNetwork($this->getEm(), 'network:Agglobus', 'Centre');
-        $user = $this->getEm()->getRepository('CanalTPSamEcoreUserManagerBundle:User')->find(1);
-        $network->addUser($user);
-        $this->getEm()->flush();
-    }
-
     public function testNetworkSwitch()
     {
         $route = $this->generateRoute('canal_tp_mtt_homepage');
         $crawler = $this->client->request('GET', $route);
-        $this->insertNetwork();
         $firstMenuActive = $crawler->filter('#application-navbar ul.nav.navbar-nav > li.dropdown ul li.first.active')->count();
         $this->assertTrue($firstMenuActive == 1, "First menu item in perimeter menu should be active. Expected 1. Count :$firstMenuActive");
         $route = $this->generateRoute(
