0453b1752e34d77a23a7122277259da5f2b51dac
root <root@srv-dev03.(none)>
2014-12-05 17:45:27 +0100

    Prise en compte du paramètre token lors de l'insertion des fixtures. (Temporaire)

--- DataFixtures/ORM/FixturesCustomer.php
+++ DataFixtures/ORM/FixturesCustomer.php
@@ -5,18 +5,34 @@ namespace CanalTP\MttBundle\DataFixtures\ORM;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use CanalTP\SamCoreBundle\DataFixtures\ORM\CustomerTrait;
 
 
-class FixturesCustomer extends AbstractFixture implements OrderedFixtureInterface
+class FixturesCustomer extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
 {
     use CustomerTrait;
 
+    /**
+    * @var ContainerInterface
+    */
+    private $container;
+
+    /**
+    *{@inheritDoc}
+    */
+    public function setContainer(ContainerInterface $container = null)
+    {
+         $this->container = $container;
+    }
+
     public function load(ObjectManager $om)
     {
-        $this->addCustomerToApplication($om, 'app-mtt', 'customer-canaltp');
-        $this->addPerimeterToCustomer($om, 'fr-cen', 'network:Agglobus', 'customer-canaltp');
-        $this->addPerimeterToCustomer($om, 'fr-cen', 'network:Filbleu', 'customer-canaltp');
+        $navitiaToken = $this->container->getParameter('nmm.navitia.token');
+        $this->addCustomerToApplication($om, 'app-mtt', 'customer-canaltp',$navitiaToken);
+
+        $this->addPerimeterToCustomer($om, 'tisseo', 'network:tisseo', 'customer-canaltp');
         $om->flush();
     }
 
