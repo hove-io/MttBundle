a39bf49bc7ae9a1c73f83b00728dfd3f5e432d84
David Quintanel <david.quintanel@canaltp.fr>
2015-06-18 17:10:10 +0200

    fixed previous test

--- Tests/Functional/Controller/TimetableControllerTest.php
+++ Tests/Functional/Controller/TimetableControllerTest.php
@@ -95,7 +95,12 @@ class TimetableControllerTest extends AbstractControllerTest
     private function checkCodeBlockInTimetableViewPage($translator, $seasonId)
     {
         $crawler = $this->doRequestRoute($this->getRoute('canal_tp_mtt_timetable_view', $seasonId));
-        $this->assertEmpty($crawler->filter('div#text_block_4 div.content')->text());
+
+        $this->assertEquals(
+            '654895',
+            $crawler->filter('div#text_block_4 div.content')->text(),
+            "Stop point code totem not found in stop point timetable view page"
+        );
 
         $crawler = $this->doRequestRoute($this->getRoute('canal_tp_mtt_timetable_edit', $seasonId, false));
         $this->assertEquals(
