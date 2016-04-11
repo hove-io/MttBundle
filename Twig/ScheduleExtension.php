089a1f04269afaea4044afa2e8abab96af5fa921
tcy <thomas.chevily@canaltp.fr>
2016-04-11 18:44:46 +0200

    Updating calendar notation

--- Twig/ScheduleExtension.php
+++ Twig/ScheduleExtension.php
@@ -30,43 +30,113 @@ class ScheduleExtension extends \Twig_Extension
 
     public function scheduleFilter($journey, $notes, $notesType = LayoutConfig::NOTES_TYPE_EXPONENT, $calendar = false)
     {
-        $value = date('i', $journey->date_time->getTimestamp());
+        $minute = date('i', $journey->date_time->getTimestamp());
 
         if (count($journey->links) < 1) {
-            return $value;
+            return $minute;
         }
+
+        $colors     = []; $exposents  = [];
 
         foreach ($journey->links as $link) {
-            if ($this->linkIsDecorable($link)) {
-                $noteIndex = $this->findNoteIndex($link->id, $notes, $calendar);
-                $this->decorateMinute($notesType, $notes, $noteIndex, $value);
+            if (!$this->linkIsDecorable($link)) {
+                continue;
             }
+
+            $noteIndex = $this->findNoteIndex($link->id, $notes, $calendar);
+
+            if ($notesType == LayoutConfig::NOTES_TYPE_COLOR) {
+                $colors[]   = $this->getColor($notes, $noteIndex);
+            }
+            $exposents[]= $this->getExposent($noteIndex);
+
         }
 
-        return $value;
+        $colorizedMinute = $this->colorizeMminute($minute, $colors);
+        return $this->addExposants($colorizedMinute, $exposents, $notesType);
     }
 
     /**
-     * Decorates minute string with html elements
+     * Retrieves color for note index
      *
-     * (Adds superclase or/and background color)
+     * @param array $notes
+     * @param Integer $noteIndex
      *
-     * @param string  $notesType
-     * @param array   $notes
-     * @param integer $noteIndex
-     * @param string  $minute
-     *
-     * @return string
+     * @return string | Null
      */
-    private function decorateMinute($notesType, $notes, $noteIndex, &$minute)
+    private function getColor(array $notes, $noteIndex)
     {
-        if ($notesType == LayoutConfig::NOTES_TYPE_COLOR) {
-            $minute = $this->decorateMinuteWithColor($notes, $noteIndex, $minute);
-        } else {
-            $minute.= $this->addFootNote($noteIndex);
+        if(!is_integer($noteIndex) || !array_key_exists($noteIndex, $notes)) {
+            return null;
         }
 
-        return $minute;
+        $noteObj = $notes[$noteIndex];
+        if(!is_object($noteObj) || !property_exists($noteObj, 'color')) {
+            return null;
+        }
+
+        return $noteObj->color;
+    }
+
+    /**
+     * Retrieves exposent by note index
+     *
+     * @param Integer $noteIndex
+     * @return Sting | Null
+     */
+    private function getExposent($noteIndex)
+    {
+        $footNote = $this->getFootNote($noteIndex);
+        if(empty($footNote)) {
+           return null;
+        }
+
+        return $footNote;
+    }
+
+    /**
+     * Adds background color to the minute string
+     *
+     * @param string $minute
+     * @param array $colors
+     * @return String
+     */
+    private function colorizeMminute($minute = '', array $colors = [])
+    {
+        $cleanedColors = array_filter($colors);
+
+        if(empty($cleanedColors)) {
+            return $minute;
+        }
+
+        return sprintf('<span style="background-color: %s">%s</span>', current($cleanedColors), $minute);
+
+    }
+
+    /**
+     * Adds exposents to the minute string if note type isnot color
+     *
+     * @param string $minute
+     * @param array $exposents
+     * @param type $notesType
+     *
+     * @return String
+     */
+    private function addExposants($minute = '', array $exposents = [], $notesType)
+    {
+        $cleanedExposents = array_filter($exposents);
+
+        $exposantsNb = count($cleanedExposents);
+
+        if($notesType == LayoutConfig::NOTES_TYPE_COLOR && $exposantsNb < 2) {
+            return $minute;
+        }
+
+        array_walk($cleanedExposents, function(&$value) {
+            $value = sprintf('<sup>%s</sup>', $value);
+        });
+
+        return $minute . join('', $cleanedExposents);
     }
 
     /**
@@ -87,53 +157,27 @@ class ScheduleExtension extends \Twig_Extension
         return $link->type == 'notes' || $link->type == 'exceptions';
     }
 
-    /**
-     * Adds superscript element to $value if $index is not false
-     *
-     * @param integer $noteIndex
-     *
-     * @return string
-     */
-    private function addFootNote($noteIndex)
-    {
-        $footNote = $this->footnoteFilter($noteIndex);
-        if(empty($footNote)) {
-           return '';
-        }
-
-        return sprintf('<sup>%s</sup>', $footNote);
-    }
-
-    /**
-     * Adds background color to the $value if color could be found in $notes
-     *
-     * @param array $notes
-     * @param integer $noteIndex
-     * @param String $value
-     *
-     * @return String
-     */
-    private function decorateMinuteWithColor($notes, $noteIndex, $value)
-    {
-        if(!is_integer($noteIndex)) {
-            return $value;
-        }
-
-        if(!array_key_exists($noteIndex, $notes)) {
-            return $value;
-        }
-
-        $noteObj = $notes[$noteIndex];
-        if(!is_object($noteObj) || !property_exists($noteObj, 'color')) {
-            return $value;
-        }
-
-        return sprintf('<span style="background-color: %s">%s</span>', $noteObj->color, $value);
-    }
-
-    public function footnoteFilter($index)
+    private function getFootNote($index)
     {
         return $index === false ? '' : chr($this->ascii_start + $index);
+    }
+
+    public function footnoteFilter($index, $note, $notesType = LayoutConfig::NOTES_TYPE_EXPONENT)
+    {
+
+        if($index === false) {
+            return '';
+        }
+
+        if(!is_object($note) || !property_exists($note, 'color')) {
+            return $this->getFootNote($index);
+        }
+
+        if($notesType == LayoutConfig::NOTES_TYPE_EXPONENT) {
+            return $this->getFootNote($index);
+        }
+
+        return sprintf('<span style="background-color: %s" class="label"> </span> ',$note->color);
     }
 
     public function calendarMax($calendar, $min = 12)
